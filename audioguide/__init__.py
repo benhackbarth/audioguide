65f293534674bbef0b08b0bd241afd2f99b00ad5
benhackbarth <hackbarth@gmail.com>
2014-08-30 15:59:25 +0100

    increment version number

--- audioguide/__init__.py
+++ audioguide/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Benjamin Hackbarth, Norbert Schnell, Philippe Esling, Diemo Schwarz, Gilbert Nouno"
 __author_email__ = "b.hackbarth@liv.ac.uk"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 import sys, os, platform
 	
