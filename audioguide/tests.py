e097e7d33abc966fb42421816bbe282ac1b8148b
benhackbarth <hackbarth@gmail.com>
2018-03-27 17:44:33 +0200

    oops

--- audioguide/tests.py
+++ audioguide/tests.py
@@ -117,6 +117,7 @@ UserVar_types = {
 'CSOUND_PLAY_RENDERED_FILE': ['True or False'],
 'CSOUND_RENDER_DUR': ['cps', 'tgt'],
 'CSOUND_NORMALIZE': ['True or False'],
+'CSOUND_NORMALIZE_PEAK_DB': ['a number'],
 #################  DESCRIPTOR COMPUTATION SETTINGS  ################
 'DESCRIPTOR_OVERRIDE_DATA_PATH': ['None', 'a string'],
 'DESCRIPTOR_FORCE_ANALYSIS': ['True or False'],
