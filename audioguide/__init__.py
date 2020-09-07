fixed bug in normalization; new desscriptors now output identical results

audioguide/__init__.py
@@ -35,52 +35,88 @@ def optionsfiletest(argv):
 
 
 
-class concatenate:
+class main:
+	from audioguide import concatenativeclasses, simcalc, userinterface, util, descriptordata, anallinkage, musicalwriting
 
-	def __init__(self, ):
-		defaultpath, libpath = setup(os.path.dirname(__file__))
-		opspath = optionsfiletest(sys.argv)
+	def __init__(self):
+		if sys.maxsize > 2**32: bits = 64
+		else: bits = 32
+		libpath = os.path.join(os.path.dirname(__file__), 'audioguide', 'pylib%i.%i-%s-%i'%(sys.version_info[0], sys.version_info[1], platform.system().lower(), bits))
 		sys.path.append(libpath)
-		# import audioguide's submodules
-		from audioguide import sfsegment, concatenativeclasses, simcalc, userinterface, util, descriptordata, anallinkage, musicalwriting
-		# import other modules
-		import numpy as np
-		import json
-		
+
+
+	def initialize_options(self, opsdata):
+		'''opsdata may be a dict or filepath string'''
+		opspath = optionsfiletest(sys.argv)
 		###########################################
 		## LOAD OPTIONS AND SETUP SDIF-INTERFACE ##
 		###########################################
 		ops = concatenativeclasses.parseOptions(opsfile=opspath, defaults=defaultpath, scriptpath=os.path.dirname(__file__))
 		if 'concateMethod' in ops.EXPERIMENTAL and ops.EXPERIMENTAL['concateMethod'] == 'framebyframe':
 			util.error("CONFIG", "Frame by frame concatenation is only possible with the agConcatenateFrames.py script.")
-
-
 		p = userinterface.printer(ops.VERBOSITY, os.path.dirname(__file__), ops.HTML_LOG_FILEPATH)
 		p.printProgramInfo(audioguide.__version__)
 		AnalInterface = ops.createAnalInterface(p)
-		p.middleprint('SOUNDFILE CONCATENATION')
 
 
+	def load_target():
+		############
+		## TARGET ##
+		############
+		p.logsection( "TARGET" )
+		tgt = audioguide.sfsegment.target(ops.TARGET, AnalInterface)
+		tgt.initAnal(AnalInterface, ops, p)
+		tgt.stageSegments(AnalInterface, ops, p)
+
+		# experimental
+		#tgt.peakdata = AnalInterface.analize_spectralPeaks(tgt.filename, fftsize=2048)
+		#for s in tgt.segs:
+		#	s.peaks = []
+		#	for time, partials in tgt.peakdata['peaks']:
+		#		if time >= s.segmentStartSec and time <= s.segmentEndSec:
+		#			s.peaks.extend(partials)
+
+		if len(tgt.segs) == 0:
+			util.error("TARGET FILE", "no segments found!  this is rather strange.  could your target file %s be digital silence??"%(tgt.filename))
+		p.log("TARGET SEGMENTATION: found %i segments with an average length of %.3f seconds"%(len(tgt.segs), np.average(tgt.seglengths)))
+		#######################
+		## target label file ##
+		#######################
+		if ops.TARGET_SEGMENT_LABELS_FILEPATH != None:
+			tgt.writeSegmentationFile(ops.TARGET_SEGMENT_LABELS_FILEPATH)
+			p.log( "TARGET: wrote segmentation label file %s"%ops.TARGET_SEGMENT_LABELS_FILEPATH )
+		#############################
+		## target descriptors file ##
+		#############################
+		if ops.TARGET_DESCRIPTORS_FILEPATH != None:
+			outputdict = tgt.whole.desc.getdict()
+			outputdict['frame2second'] = AnalInterface.f2s(1)
+			fh = open(ops.TARGET_DESCRIPTORS_FILEPATH, 'w')
+			json.dump(outputdict, fh)
+			fh.close()
+			p.log("TARGET: wrote descriptors to %s"%(ops.TARGET_DESCRIPTORS_FILEPATH))
+		##############################
+		## target descriptor graphs ##
+		##############################
+		if ops.TARGET_PLOT_DESCRIPTORS_FILEPATH != None:
+			tgt.plotMetrics(ops.TARGET_PLOT_DESCRIPTORS_FILEPATH, AnalInterface, p)
+		###############################
+		## target segmentation graph ##
+		###############################
+		if ops.TARGET_SEGMENTATION_GRAPH_FILEPATH != None:
+			tgt.plotSegmentation(ops.TARGET_SEGMENTATION_GRAPH_FILEPATH, AnalInterface, p)
+
+		descriptors = []
+		dnames = []
+		for dobj in AnalInterface.requiredDescriptors:
+			if dobj.seg or dobj.name in ['power']: continue
+			d = np.array(tgt.whole.desc[dobj.name][:])
+			d -= np.min(d)
+			d /= np.max(d)
+			d = np.around(d, 2)
+	
+			descriptors.append(d)
+			dnames.append(dobj.name)
+		p.html.jschart_timeseries(yarray=np.array([AnalInterface.f2s(i) for i in range(tgt.whole.lengthInFrames)]), xarrays=descriptors, ylabel='time in seconds', xlabels=dnames)
 
 
-	def initalize_target(self, ):
-		pass
-
-
-	def initalize_corpus(self, ):
-		pass
-
-
-	def normalize(self, ):
-		pass
-
-	def initalize_concatenate(self, ):
-		pass
-
-
-	def concatenate(self, ):
-		pass
-
-
-	def render(self, ):
-		pass

audioguide/descriptordata.py
@@ -89,6 +89,7 @@ class descriptor_manager:
 		self.desc_objs.sort(key=lambda x: x.tag)
 		self.desc_obj_tags = [o.tag for o in self.desc_objs]
 		self.norm_tags = list(set(self.desc_obj_tags))
+		self.norm_tags.sort() # make sure they are ordered!
 		self.norm_coeff_tag_dict = {t: {} for t in self.norm_tags}
 		self.tv_length = sum([o.get('effDurFrames-seg') for o in self.desc_objs])
 		self.seg_length = len(self.desc_objs)
@@ -220,8 +221,7 @@ class descriptor_manager:
 				if not mat.has_column(dname):
 					# on demand calculation of new columns - delta, deltadelta, odf
 					mat.calculate_new_column(dname, dparams['type'], dparams['parent'])
-				idx = self.start
-			#print(self.tag, st, idx, length, dname, start, stop)
+				idx = start + self.start
 			return mat, idx, idx+length
 		#####################################	
 		def get(self, dname, start=0, stop=None, norm=False, mixture=False, copy=False):
@@ -281,14 +281,9 @@ class descriptor_manager:
 			self.segmented_dataspace.clear()
 			self.segmented_norm_dataspace.clear()
 		#####################################	
-		def mixture_mix(self, cpsseg, ampscale, minLen, descriptors, myfile, verbose=True):
+		def mixture_mix(self, cpsseg, ampscale, minLen, descriptors, verbose=True):
 			mix_dur = min(self.len-self.sfseghandle.seek, cpsseg.lengthInFrames)
 			tgtseek = self.sfseghandle.seek
-			print()
-			print()
-			myfile.write("%i %i %i\n"%(self.sfseghandle.idx, self.sfseghandle.seek, mix_dur))
-			print()
-			print()
 			for d in descriptors:
 				dparams = self.overlord.descriptor_string_digest(d.name)
 				if dparams['isseg']:
@@ -309,29 +304,6 @@ class descriptor_manager:
 
 
 
-
-def timeVaryingDescriptorMixture(tgtsegh, tgtseek, cpssegh, cpsseek, dobj, cpsAmpScale, v=False):
-	mix_dur = min(tgtsegh.lengthInFrames-tgtseek, cpssegh.lengthInFrames-cpsseek)
-	tgt_vals = tgtsegh.mixdesc[dobj.name][tgtseek:tgtseek+mix_dur]
-	cps_vals = cpssegh.desc[dobj.name][cpsseek:cpsseek+mix_dur]
-	if dobj.describes_energy: # powers are summed
-		mixture = tgt_vals + (cps_vals*cpsAmpScale)
-	elif dobj.name == 'zeroCross': 
-		mixture = np.maximum(tgt_vals, cps_vals)	
-	else: # power averaged
-		tgt_amps = tgtsegh.mixdesc['power'][tgtseek:tgtseek+mix_dur]
-		cps_amps = cpssegh.desc['power'][cpsseek:cpsseek+mix_dur]*cpsAmpScale
-		mixture = ((tgt_vals*tgt_amps)+(cps_vals*cps_amps))/(tgt_amps + cps_amps)	
-	if v: # verbose printing
-		maxxy = min(5, len(mixture))
-		if dobj.describes_energy: # powers are summed
-			print("SUM", dobj.name, tgtseek, mix_dur)
-		else:
-			print("MIXTURE", dobj.name, tgtseek, mix_dur)
-		print("\tpastmix:", tgt_vals[0:maxxy])
-		print("\tcorpus:", cps_vals[0:maxxy])
-		print("\tnewmix:", mixture[0:maxxy])
-	return mixture
 
 
 
@@ -395,19 +367,6 @@ def TimevaryingDescriptorComputation(origdata, dname, type):
 
 
 
-def odf(powers, numberaverage):
-	# with Norbert Schnell
-	# 	an averaged delta function
-	matrixe = np.zeros((numberaverage, len(powers))) # matrix of length x number average
-	for frame in range(len(powers)):
-		for i in range(1, numberaverage+1):
-			if frame-i >= 0: matrixe[i-1][frame] = powers[frame-i] # fill it
-	medians = np.median(matrixe, axis=0)
-	newdata = powers-medians
-	return newdata
-
-
-
 def soundSegmentClassification(descriptors, segObjs, numbClasses=4):
 	try:
 		from sklearn.cluster import KMeans
@@ -423,6 +382,20 @@ def soundSegmentClassification(descriptors, segObjs, numbClasses=4):
 	kmeans.fit(scaleddata)
 	y_km = kmeans.fit_predict(data)
 	return y_km
+
+
+
+
+def odf(powers, numberaverage):
+	# with Norbert Schnell
+	# 	an averaged delta function
+	matrixe = np.zeros((numberaverage, len(powers))) # matrix of length x number average
+	for frame in range(len(powers)):
+		for i in range(1, numberaverage+1):
+			if frame-i >= 0: matrixe[i-1][frame] = powers[frame-i] # fill it
+	medians = np.median(matrixe, axis=0)
+	newdata = powers-medians
+	return newdata
 
 
 

audioguide/sfsegment.py
@@ -180,40 +180,9 @@ class targetSegment(sfsegment):
 		self.classification = 0
 		self.numberSelectedUnits = 0
 		self.idx = segmentidx
-	#################################
-#	def mixSelectedSamplesDescriptors(self, cpsh, cpsAmpScale, tgtsegSeek, AnalInterface, v=True):
-#		mix_dur = min(self.lengthInFrames-tgtsegSeek, cpsh.lengthInFrames)
-#		for d in AnalInterface.mixtureDescriptors:
-#			if not d.seg: # is time-varying
-#				self.mixdesc[d.name][tgtsegSeek:tgtsegSeek+mix_dur] = timeVaryingDescriptorMixture(self, tgtsegSeek, cpsh, 0, d, cpsAmpScale)
-#			else: # is segmented
-#				self.mixdesc[d.name].clear()
-#		self.has_been_mixed = True
 
 
 
-#def timeVaryingDescriptorMixture(tgtsegh, tgtseek, cpssegh, cpsseek, dobj, cpsAmpScale, v=False):
-#	mix_dur = min(tgtsegh.lengthInFrames-tgtseek, cpssegh.lengthInFrames-cpsseek)
-#	tgt_vals = tgtsegh.mixdesc[dobj.name][tgtseek:tgtseek+mix_dur]
-#	cps_vals = cpssegh.desc[dobj.name][cpsseek:cpsseek+mix_dur]
-#	if dobj.describes_energy: # powers are summed
-#		mixture = tgt_vals + (cps_vals*cpsAmpScale)
-#	elif dobj.name == 'zeroCross': 
-#		mixture = np.maximum(tgt_vals, cps_vals)	
-#	else: # power averaged
-#		tgt_amps = tgtsegh.mixdesc['power'][tgtseek:tgtseek+mix_dur]
-#		cps_amps = cpssegh.desc['power'][cpsseek:cpsseek+mix_dur]*cpsAmpScale
-#		mixture = ((tgt_vals*tgt_amps)+(cps_vals*cps_amps))/(tgt_amps + cps_amps)	
-#	if v: # verbose printing
-#		maxxy = min(5, len(mixture))
-#		if dobj.describes_energy: # powers are summed
-#			print("SUM", dobj.name, tgtseek, mix_dur)
-#		else:
-#			print("MIXTURE", dobj.name, tgtseek, mix_dur)
-#		print("\tpastmix:", tgt_vals[0:maxxy])
-#		print("\tcorpus:", cps_vals[0:maxxy])
-#		print("\tnewmix:", mixture[0:maxxy])
-#	return mixture
 
 
 
@@ -501,6 +470,8 @@ def segmentationAlgoV2(threshold_onset, threshold_offset, riseratio, powers, odf
 	return segments, logic
 
 
+
+
 def getDescriptorStatistics(listOfSegmentObjs, descriptorObj, takeOnlyEffDur=True, stdDeltaDegreesOfFreedom=0):
 	if descriptorObj.seg:
 		allDescs = np.array([sfsObj.desc.get(descriptorObj.name) for sfsObj in listOfSegmentObjs])
@@ -516,14 +487,4 @@ def getDescriptorStatistics(listOfSegmentObjs, descriptorObj, takeOnlyEffDur=Tru
 	return { 'min': np.min(allDescs), 'max': np.max(allDescs), 'mean': np.mean(allDescs),'stddev': np.std(allDescs, ddof=stdDeltaDegreesOfFreedom) }
 
 
-#
-#
-#def applyDescriptorNormalisation(listOfSegmentObjs, dobj, descStatistics):
-#	# this function must be unique for the corpus and target as they will have different normalization coefs
-#	normdatadict = {'method': dobj.normmethod}
-#	normdatadict.update(descStatistics)
-#	normdatadict['range'] = normdatadict['max']-normdatadict['min']
-#
-#	for sfseg in listOfSegmentObjs:
-#		sfseg.desc[dobj.name].setNorm(normdatadict)
-#
+