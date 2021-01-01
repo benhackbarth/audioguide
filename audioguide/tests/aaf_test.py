0e05ca8d6752f1b62c7347ac827e1f2a194ca678
benhackbarth <hackbarth@gmail.com>
2021-01-01 22:06:04 +0100

    added tests

---
+++ audioguide/tests/aaf_test.py
@@ -0,0 +1,26 @@
+import os, aaf2
+
+
+f = aaf2.open('/Users/ben/Desktop/test.aaf', "w")
+aaf_sr = 44100
+
+my_sf_meta = {'streams': [{'index': 0, 'codec_type': 'audio', 'sample_rate': '44100', 'channels': 1, 'duration_ts': 502892}], 'format': {'filename': '/Users/ben/Documents/audioguide/examples/cage.aiff', 'format_name': 'aiff'}}
+
+master_mob, source_mob, tape_mob = f.content.link_external_wav(my_sf_meta)
+
+for i in range(10):
+	sequence = f.create.Sequence(media_kind="sound")
+	sequence.components.append(f.create.Filler("sound", int(aaf_sr*0.5))) # silence
+	sequence.components.append(master_mob.create_source_clip(slot_id=1, start=0, length=int(aaf_sr*0.5))) # sound
+	comp_mob = f.create.CompositionMob("track %i"%i)
+	f.content.mobs.append(comp_mob)
+	timeline_slot = comp_mob.create_timeline_slot("%i" % aaf_sr)
+	timeline_slot.name = "track %i"%i
+	timeline_slot.segment = sequence	
+
+f.close()
+	
+
+
+
+
