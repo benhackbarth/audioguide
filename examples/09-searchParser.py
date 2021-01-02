57b2a8198c8da313b48fee9339334c474df2f9ad
benhackbarth <hackbarth@gmail.com>
2021-01-02 15:40:18 +0100

    better example

--- examples/09-searchParser.py
+++ examples/09-searchParser.py
@@ -1,6 +1,9 @@
 TARGET = tsf('cage.aiff', thresh=-26, offsetRise=2)
 
-CORPUS = [csf('heat sink.aiff'), csf('lachenmann.aiff'),]
+CORPUS = [
+csf('lachenmann.aiff', limit='centroid-seg > 90%'), # doughnut
+csf('lachenmann.aiff', limit='centroid-seg < 10%'), #  corpus
+]
 
 ################################################################################
 ## The parser spass() method toggles between two different actions            ##
@@ -43,20 +46,12 @@ CORPUS = [csf('heat sink.aiff'), csf('lachenmann.aiff'),]
 ################################################################################
 
 
-#SEARCH = [
-## for the loudest 50% of target segements, use corpus ID 1; otherwise use ID 2
-#spass('parser', 'power-seg > 50%', 'corpus_select', [0], [1]),
-#spass('closest_percent', d('effDur-seg', norm=1), d('power-seg', norm=1), percent=20),
-#spass('closest', d('mfccs'))
-#]
-
-
 
 SEARCH = [
-spass('closest_percent', d('effDur-seg', norm=1), d('power-seg', norm=1), percent=20),
-spass('parser', 'zeroCross-seg > 50%', 'closest_percent', [d('flatnesses-seg')], [d('centroid-seg')], percent=20), # for the 50% of target segments with the highest zerocrossing, pick the best 20% of remaining corpus sounds using flatnesses; otherwise use centroid
+# for the 50% of target segments with the highest zerocrossing, pick from cps idx 0. otherwise pick from cps idx 1
+spass('parser', 'zeroCross-seg > 50%', 'corpus_select', [0], [1]), 
 spass('closest', d('mfccs'))
 ]
 
 
-SUPERIMPOSE = si(maxSegment=1)
+SUPERIMPOSE = si(maxSegment=2)
