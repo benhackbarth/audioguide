cf27274efd7d40b40240b224c7ee56f073560cb5
benhackbarth <hackbarth@gmail.com>
2022-03-22 10:40:37 +0000

    fixed bug in reaper scableDb volume

--- audioguide/fileoutput/reaper.py
+++ audioguide/fileoutput/reaper.py
@@ -23,7 +23,7 @@ class output:
 		for trackname, trackitems, trackorigin in self.tracks:
 			track_str = ''
 			for d in trackitems:
-				track_str += '''     <ITEM\n      POSITION  %f\n      NAME %s\n      LENGTH %f\n      SOFFS %f\n      VOLPAN %f 0.0 %f -1.0 \n      FADEIN 1 %f 0.0\n      FADEOUT 1 %f 0.0\n      PLAYRATE 1.000 1 %f -1\n      <SOURCE WAVE\n        FILE "%s"\n      >\n     >\n'''%(d['time'], d['name'], d['orig_duration'], d['skip'], d['ampscale'], d['ampscale'], d['fadein'], d['fadeout'], d['transposition'], d['file'])
+				track_str += '''     <ITEM\n      POSITION  %f\n      NAME %s\n      LENGTH %f\n      SOFFS %f\n      VOLPAN %f 0.0 1.0 -1.0 \n      FADEIN 1 %f 0.0\n      FADEOUT 1 %f 0.0\n      PLAYRATE 1.000 1 %f -1\n      <SOURCE WAVE\n        FILE "%s"\n      >\n     >\n'''%(d['time'], d['name'], d['orig_duration'], d['skip'], d['ampscale'], d['fadein'], d['fadeout'], d['transposition'], d['file'])
 			all_tracks_str += '''  <TRACK\n   NAME "%s"\n%s  >\n'''%(trackname, track_str)
 		f.write('''<%s\n%s>'''%(rpp_header, all_tracks_str))
 		f.close()
